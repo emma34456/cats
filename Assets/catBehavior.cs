3db1cece27c201ea3785be7a91be51975fb11be7
emma34456 <emmayxli10@gmail.com>
2025-12-28 22:36:47 -0800

    added stamina

--- Assets/catBehavior.cs
+++ Assets/catBehavior.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class catBehavior : MonoBehaviour
 {
     [SerializeField] private float throwMult = 1f;
+    [SerializeField] private float maxStamina = 5f;
+    private float stamina;
+    public Image staminaBar;
     private Rigidbody2D rb;
     private Collider2D col;
     private Vector2 MousePos;
@@ -14,6 +18,7 @@ public class catBehavior : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        stamina = maxStamina;
     }
 
     // Update is called once per frame
@@ -30,6 +35,11 @@ public class catBehavior : MonoBehaviour
             if (dragging)
             {
                 mouseVelocity = Mouse.current.delta.ReadValue();
+                stamina -= Time.deltaTime;
+                if (stamina <= 0f)
+                {
+                    dragging = false;
+                }
             }
         }
         if (dragging && Mouse.current.leftButton.wasReleasedThisFrame)
@@ -37,6 +47,15 @@ public class catBehavior : MonoBehaviour
             dragging = false;
             rb.linearVelocity = mouseVelocity * throwMult;
         }
+        if (!dragging && stamina < maxStamina)
+        {
+            stamina += Time.deltaTime;
+        }
+        else if (stamina > maxStamina)
+        {
+            stamina = maxStamina;
+        }
+        staminaBar.fillAmount = stamina / maxStamina;
     }
     void FixedUpdate()
     {
